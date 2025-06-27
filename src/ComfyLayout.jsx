13a42aa0e30bf18ae3a8589c890f30f9da2f47d0
robert <robertrenbysanjuan@gmail.com>
2025-06-27 18:08:38 +0800

    feat: footer done

--- src/ComfyLayout.jsx
+++ src/ComfyLayout.jsx
@@ -1,6 +1,7 @@
 
 import { Outlet } from "react-router-dom"
 import Header from './components/Header'
+import Footer from "./components/Footer"
 import { useState } from "react"
 
 export default function Layout(){
@@ -12,6 +13,7 @@ export default function Layout(){
         <>
             <Header isMobile={isMobile} setIsMobile={setIsMobile}/>
             <Outlet context={{isMobile, setIsMobile}}/>
+            <Footer />
         </>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return(
+        <footer>
+            <p>© 2025 <span className="comfySloth">ComfySloth</span> ALl rights reserved</p>
+        </footer>
+    )
+}
