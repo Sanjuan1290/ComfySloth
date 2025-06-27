73d0cf873f9eaef403b20d08a6fbc9f0f0612f8c
robert <robertrenbysanjuan@gmail.com>
2025-06-27 17:09:24 +0800

    feat: news Letter done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@
 import ShopNow from "../components/HomeComponents/ShopNow"
 import Featured from "../components/HomeComponents/Featured"
 import Custom from "../components/HomeComponents/Custom"
+import Newsletter from "../components/HomeComponents/Newsletter"
 
 export default function Home(){
     
@@ -10,6 +11,7 @@ export default function Home(){
             <ShopNow />
             <Featured />
             <Custom />
+            <Newsletter />
         </>
     )
 }
---
+++ src/components/HomeComponents/Newsletter.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Newsletter(){
+
+    return(
+        <section className="newsletter_Section">
+            <h1>Join our newsletter and get 20% off</h1>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint unde quaerat ratione soluta veniam provident adipisci cumque eveniet tempore?</p>
+
+                <div>
+                    <input type="text" placeholder="Enter Email"/>
+                    <button>Subscribe</button>
+                </div>
+            </div>
+        </section>
+    )
+}
