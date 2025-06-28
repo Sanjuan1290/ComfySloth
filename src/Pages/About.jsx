f1d802e3557c83af865ce06d43ca799b8422f344
robert <robertrenbysanjuan@gmail.com>
2025-06-28 13:35:50 +0800

    feat: about done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,24 @@
 
+import { NavLink } from "react-router-dom"
 
 export default function About(){
 
     return(
         <section className="about_Section">
 
-            
+            <div>
+                <h1><NavLink to='/'>Home</NavLink> / About</h1>
+            </div>
+
+            <div>
+                <img src="/HomeImages/ShopNowImage/niceTable.png" alt="nice table" />
+
+                <div className="ourStory">
+                    <h1>Our Story</h1>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat accusantium sapiente tempora sed dolore esse deserunt eaque excepturi, delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt sequi blanditiis est exercitationem molestiae delectus saepe odio eligendi modi porro eaque in libero minus unde sapiente consectetur architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum totam velit saepe sed quos similique amet. Ex, voluptate accusamus nesciunt totam vitae esse iste.</p>
+                </div>
+            </div>
         </section>
     )
 }
